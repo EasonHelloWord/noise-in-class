20d9e9614ea8abfa8a4ac9a005a4af79717d648d
Eason_Jan <a13564674280@163.com>
2023-10-17 00:21:22 +0800

    【修复】资源文件访问

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
 from threading import Thread
 from apps import update,MicMonitor
 if __name__ == "__main__":
     current_version="V2.0.0"
+    # 资源文件目录访问
+
+
+    def source_path(relative_path):
+        # 是否Bundle Resource
+        if getattr(sys, 'frozen', False):
+            base_path = sys._MEIPASS
+        else:
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
+
+
+    # 修改当前工作目录，使得资源文件可以被正确访问
+    cd = source_path('')
+    os.chdir(cd)
     Thread(target=update.check_update,args=(current_version,)).start()
     MicMonitor.MicMonitor(current_version).run()
