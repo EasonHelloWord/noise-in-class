a1976ea283c457824b0835d32b47d79029490e9d
Eason_Jan <a13564674280@163.com>
2023-10-23 23:16:31 +0800

    【更新】版本更新

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from threading import Thread
 from apps import update,db_control
 if __name__ == "__main__":
-    current_version="V2.1.1"
+    current_version="V2.1.2"
     # 资源文件目录访问
 
 
